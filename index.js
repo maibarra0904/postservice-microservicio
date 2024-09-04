1fa475b906aebb2115a8b606063058cce1e14548
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-09-03 23:40:36 -0500

    Cambio a url kubernete creado

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/posts', async (req, res) => {
     title
   })
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'POST_CREATED',
     data: {
       id,
@@ -44,6 +44,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('v70')
+  console.log('v75')
   console.log('Listening on 4000');
 });
